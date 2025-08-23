e3f48449ac57049eb60b2f124cae3723840f94f1
ZYNXP-hz <leoneo1221@gmail.com>
2025-08-23 18:08:09 +0000

    complete challenge

--- lab-5/challenge.c
+++ lab-5/challenge.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
-int main()
-{
-    int num[5];
-    int ele;
-    printf("Enter number of elements:");
-    scanf("%d",&num);
-    for(int n=0; n<5;n++)
-    {
-        printf("Enter %dintegers:",n+1);
-        scanf("%d",&num[5]);
+
+int main() {
+    int n;
+  
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n], counted[n];
+
+   
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        counted[i] = 0;
     }
 
+    
+    for (int i = 0; i < n; i++) {
+        if (counted[i]) continue; 
+
+        int count = 1;
+
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+                counted[j] = 1; 
+            }
+        }
+
+        printf("Element %d occurs %d times\n", arr[i], count);
+    }
+
+    return 0;
 }
